ce8bc826e7447b5430d859f8987562f700e24193
jpukabeals <77247525+jpukabeals@users.noreply.github.com>
2022-12-19 16:09:22 -0600

    still need to fix R100 dataset because bulk N will be zero for stand.age 2 since no treatments were applied!!

--- new-dataset.R
+++ new-dataset.R
@@ -1,25 +1,28 @@
 
 # creating a full dataset of all possible options
 
-c("staples","v17","r100") -> site_opts
-
-c(2018:2021) -> year_opts
-
-c("none","fall","spring","summer") -> timing_opts
-
-seq(0,140,20) -> rate_opts
-
-expand.grid(
-  site_opts,
-  year_opts,
-  timing_opts,
-  rate_opts,
-  stringsAsFactors = T
-) %>% 
-  rename(site = Var1,
-         year = Var2,
-         timing = Var3,
-         rate = Var4)-> dat_full
+# c("staples","v17","r100") -> site_opts
+# 
+# c(2018:2021) -> year_opts
+# 
+# c("fall","spring","summer") -> timing_opts
+# 
+# seq(0,140,20) -> rate_opts
+# 
+# seq(1:54) -> plot_opts
+# 
+# expand.grid(
+#   site_opts,
+#   year_opts,
+#   plot_opts,
+#   timing_opts,
+#   rate_opts
+# ) %>% 
+#   rename(site = Var1,
+#          year = Var2,
+#          plot = Var3,
+#          timing = Var4,
+#          rate = Var5)-> dat_full
 
 # dat_full is all possible data we could have
 
@@ -28,17 +31,132 @@ expand.grid(
 
 # V17 ---------------------------------------------------------------------
 
+# plot = only 16
 # timing = fall spring 
 # rate = 40 for split, 80 for others
 # year = 2019:2021
 
+# 
+# 
+# dat_full %>% 
+#   # glimpse()
+#   filter(plot <= 16) %>% 
+#   filter(site == "v17") %>% 
+#   filter(year != "2018") %>% 
+#   filter(timing != "summer") %>% 
+#   filter(rate == "40" |
+#            rate == "80" | 
+#            rate == "0") 
+#   
+# fuck it, let's try this in excel
+# nevermind!
 
-dat_full %>% 
-  filter(site == "v17") %>% 
-  filter(year != "2018") %>% 
-  filter(timing != "summer") %>% 
-  filter(rate == "40" |
-           rate == "80" | 
-           rate == "0") 
-  
-  
+
+dat_v17 %>% 
+  # dplyr::select(stand.age,treatment) %>%
+  mutate(gross_rate = readr::parse_number(as.character(treatment),
+                                          na = "control")) %>% 
+  mutate(gross_rate = replace_na(gross_rate,0)) %>%
+  mutate(fall2 = if_else(
+    treatment=="80 Fall",
+    80,
+    if_else(
+      treatment == "80 Split",
+      40,
+      0
+    )
+  )) %>%
+  mutate(fall = if_else(
+    stand.age==1,
+    0,
+    fall2
+  )) %>% 
+  mutate(spring = if_else(
+    treatment=="80 Spring",
+    80,
+    if_else(
+      treatment == "80 Split",
+      40,
+      0
+    )
+  )) %>% 
+  mutate(summer = 0) %>% 
+  dplyr::select(-fall2)  -> dat_v172
+
+
+# staples -----------------------------------------------------------------
+
+# 60N in fall or spring
+# varying rates in summer
+
+dat_staples %>% 
+  # dplyr::select(stand.age,treatment) %>%
+  mutate(gross_rate = readr::parse_number(as.character(treatment),
+                                          na = "control")) %>% 
+  mutate(gross_rate = replace_na(gross_rate,0)) %>% 
+  mutate(detect_fall = str_detect(treatment,"fall"),
+         detect_spring = str_detect(treatment,"spring")) %>% 
+  mutate(fall2 = if_else(
+    detect_fall == T,
+    60,
+    0
+  ),
+  spring = if_else(
+    detect_spring == T,
+    60,
+    0
+  )
+  ) %>%  
+  mutate(summer = gross_rate-fall2-spring) %>% 
+  mutate(fall = if_else(
+    stand.age == "1",
+    0,
+    fall2
+  ),
+  .before=spring
+  ) %>% 
+  dplyr::select(-c(detect_fall,detect_spring,fall2)) -> dat_staples2
+
+
+# R100 --------------------------------------------------------------------
+
+# should be same as staples
+# except no N rate in year 2
+
+dat_r100 %>% 
+  # dplyr::select(stand.age,treatment) %>%
+  mutate(gross_rate = readr::parse_number(as.character(treatment),
+                                          na = "control")) %>% 
+  mutate(gross_rate = replace_na(gross_rate,0)) %>% 
+  mutate(detect_fall = str_detect(treatment,"fall"),
+         detect_spring = str_detect(treatment,"spring")) %>% 
+  mutate(fall2 = if_else(
+    detect_fall == T,
+    60,
+    0
+  ),
+  spring = if_else(
+    detect_spring == T,
+    60,
+    0
+  )
+  ) %>%  
+  mutate(summer = gross_rate-fall2-spring) %>% 
+  mutate(fall = if_else(
+    stand.age == "1",
+    0,
+    fall2
+  ),
+  .before=spring
+  ) %>% 
+  dplyr::select(-c(detect_fall,detect_spring,fall2)) -> dat_r1002
+
+
+# bind together -----------------------------------------------------------
+
+# what is the difference between a spring split and a fall split in a site where
+# there is already a summer application?
+
+dat_v172 %>% 
+  bind_rows(dat_r1002) %>% 
+  bind_rows(dat_staples2) -> dat_new
